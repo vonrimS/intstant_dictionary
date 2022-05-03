f2ff89b9836cecc67dcfe14da12e9f556e01fdf8
vonrimS <mr.mytab@gmail.com>
2022-05-03 18:24:54 +0200

    Create nav menu for Home page

--- webapp/home.py
+++ webapp/home.py
@@ -2,11 +2,38 @@ import justpy as jp
 
 
 class Home:
-    path = '/home'
+    path = '/'
 
-    def serve(self):
+    @classmethod
+    def serve(cls, req):
         wp = jp.QuasarPage(tailwind=True)
-        div = jp.Div(a=wp, classes='bg-gray-200 h-screen')
+
+        layout = jp.QLayout(a=wp, view='hHh lpR fFf')
+        header = jp.QHeader(a=layout)
+        toolbar = jp.QToolbar(a=header)
+
+        drawer = jp.QDrawer(a=layout, show_if_above=True, v_model='Left', bordered=True)
+
+        scroller = jp.QScrollArea(a=drawer, classes='fit')
+
+        qList = jp.QList(a=scroller)
+        a_classes = 'p-2 m-2 text-lg text-blue-400 hover:text-blue-700'
+        jp.Br(a=qList)
+        jp.Br(a=qList)
+        jp.A(a=qList, text='Home', href='/', classes=a_classes)
+        jp.Br(a=qList)
+        jp.Br(a=qList)
+        jp.A(a=qList, text='About', href='/about', classes=a_classes)
+        jp.Br(a=qList)
+        jp.Br(a=qList)
+        jp.A(a=qList, text='Dictionary', href='/dictionary', classes=a_classes)
+
+        jp.QBtn(a=toolbar, dense=True, flat=True, round=True, icon='menu', click=cls.move_drawer, drawer=drawer)
+        jp.QToolbarTitle(a=toolbar, text='Instant Dictionary')
+
+        container = jp.QPageContainer(a=layout)
+
+        div = jp.Div(a=container, classes='bg-gray-200 h-screen')
         jp.Div(a=div, text='This is the Home page!', classes='text-4xl m-2')
         jp.Div(a=div, text="""
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque quis euismod neque. Etiam quis est a sem dignissim dignissim sit amet non urna. Aliquam ut convallis quam. Aenean eu rutrum neque. Nullam facilisis lobortis lacus, eget accumsan mauris tincidunt non. Maecenas sit amet leo mi. Sed malesuada nisi id enim placerat, sit amet ullamcorper purus tincidunt. Nam elementum dictum lorem in tempus. Phasellus porta odio id quam facilisis varius. Phasellus lectus nisl, convallis et sem et, faucibus dictum dolor. Mauris lorem ante, vestibulum nec dictum vel, elementum sit amet nibh.
@@ -15,4 +42,9 @@ class Home:
                 """, classes='text-lg')
         return wp
 
-
+    @staticmethod
+    def move_drawer(widget, msg):
+        if widget.drawer.value == True:
+            widget.drawer.value = False
+        else:
+            widget.drawer.value = True
